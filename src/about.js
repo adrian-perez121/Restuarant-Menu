dfb80654c1cc7ce9f3b4e5abc3bd5d65fb11dfd2
Adrian Perez <appperez@ucdavis.edu>
2024-12-26 23:34:37 -0800

    Add limited functionality for the about button

--- src/about.js
+++ src/about.js
@@ -1,13 +1,31 @@
 let test = document.createElement("div");
 const content = document.querySelector(".content")
+
+function createPlanNode() {
+  const thePlan = document.createElement("div");
+  const heading = document.createElement("h2");
+  const description = document.createElement("span");
+
+
+  heading.textContent = "The Plan";
+  description.textContent = "We hope to go international so that we can reach doggys all over the world. But for now we are going to stick to trying to expand over California. After all, thats the population with the most doggys currently."
+
+  thePlan.appendChild(heading);
+  thePlan.appendChild(description);
+
+  return thePlan;
+}
+
 export class About {
   constructor() {
-    let test = document.createElement("div");
-    test.classList.add("about");
-    test.textContent = "Jello this is a test";
-    const content = document.querySelector(".content");
-    content.appendChild(test);
-    
+    this.contentContainer = document.querySelector(".content");
+    this.thePlan = createPlanNode();
   }
+
+  appendToContent = () => {
+    this.contentContainer.appendChild(this.thePlan);
+
+  }
+
 }
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import "./styles.css";
+import { About } from "./about";
+
+const AboutPage = new About;
+const AboutButton = document.querySelector("#about-btn")
+
+AboutButton.addEventListener("click", AboutPage.appendToContent)
 
